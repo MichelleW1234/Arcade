494f59feefcc1a6d463f2c3e389fac4eb1fa7077
Michelle Wee <michellew1822@gmail.com>
2025-01-02 13:52:43 -0800

    Changed CSS labels and file names

--- src/components/ArcadeStartScreen.jsx
+++ src/components/ArcadeStartScreen.jsx
@@ -7,11 +7,11 @@ function ArcadeStartScreen (){
         <div className = "startScreen">
 
             <h1 className = "intro">
-                Hello. You are about to enter The Arcade. Are you <span className = "introGlitch">ready</span>?
+                Welcome to The <span className = "introGlitch">Arcade</span>.
             </h1>
 
             <a href="/selection">
-                <button className = "enterArcadeButton"> Enter </button>
+                <button className = "startButton"> Enter </button>
             </a>
             
         </div>

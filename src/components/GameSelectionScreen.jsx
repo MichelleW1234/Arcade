bfdcfe483df1d08fb44be73199f5fdd9cc577de9
Michelle Wee <michellew1822@gmail.com>
2025-01-05 15:25:23 -0800

    designing game selection screen

--- src/components/GameSelectionScreen.jsx
+++ src/components/GameSelectionScreen.jsx
@@ -4,9 +4,27 @@ import "./GameSelectionScreen.css";
 function GameSelecionScreen (){
 
     return (
-        <div className = "selectionScreen">
+        <div className = "ArcadeselectionScreen">
             
-            <h1>Choose a game: </h1>
+            <h1 className = "intro"> Choose a game: </h1>
+
+
+            <div className = "ArcadeGameBoard">
+
+                <div className = "ArcadeGameContainer">
+
+                    <div className = "ArcadeGame"> 
+                        
+                        <h2> game 1 (placeholder)</h2>
+                        <h3> game description: </h3>
+                        
+                    </div>
+
+                    <button> Select </button>
+
+                </div>
+
+            </div>
 
             <a href="/start">
                 <button > Go to game </button>

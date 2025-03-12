a343462b51e56fc08cc6e20c6cddc900514dbd2f
Michelle Wee <michellew1822@gmail.com>
2025-03-12 02:59:34 -0700

    edited

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+
 import { ActiveGameProvider } from './Providers/ActiveGameProvider.jsx'; 
 import { PlayerProvider } from './Providers/PlayerProvider.jsx'; 
 

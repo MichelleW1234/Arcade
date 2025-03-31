289115a4e4c3eff0e9f73a97b87f8692ba37ff83
Michelle Wee <michellew1822@gmail.com>
2025-03-31 10:44:50 -0700

    more snake game

--- src/SNKgame/components/ResultsScreen.jsx
+++ src/SNKgame/components/ResultsScreen.jsx
@@ -1,20 +1,64 @@
 import { Link } from 'react-router-dom';
 
+import { usePlayer } from '../../Providers/PlayerProvider.jsx';
+import { useSNKUser } from '../Providers/SNKUserProvider.jsx';
+
+import { retrieveActiveGame } from '../../Helpers/helpers.js';
+
+import "./ResultsScreen.css";
+
 function ResultsScreen (){
+
+    const { Player, setPlayer} = usePlayer();
+    const { SNKUser, setSNKUser } = useSNKUser();
+
+    const transaction = () => {
+
+        const difference = (SNKUser[1]*2) - 10;
+
+        if (difference >= 0){
+
+            setPlayer(prev => [prev[0] + difference, prev[0]]);
+
+        } else {
+
+            setPlayer(prev => [0, prev[0]]);
+
+        }
+
+    }
+
+    const resetGame = () => {
+
+        setSNKUser([false, 0]);
+        transaction();
+
+    }
+
+    const reset = () => {
+
+        resetGame();
+        retrieveActiveGame(1);
+
+    }
 
     return (
 
         <div className = "screenLayout">
 
-            <h1 className = "headerwords">
-                Summary goes here.
-            </h1>
+            <div className = "SNKresultsSign"> 
 
-            <Link to="/selection" className = "generalbuttonGlitch">
+                <h1 className = "SNKresultsFont">
+                    Total Points Earned: {SNKUser[1] * 2}
+                </h1>
+
+            </div>
+
+            <Link to="/selection" className = "generalbuttonGlitch" onClick={() => reset()}>
                 Quit Game
             </Link>
 
-            <Link to="/SNKgame" className = "generalbuttonGlitch">
+            <Link to="/SNKgame" className = "generalbuttonGlitch" onClick={() => resetGame()}>
                 Play Again
             </Link>
             

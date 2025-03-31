c843957a7ee122bfbdd397c8baa1259dde721c14
Michelle Wee <michellew1822@gmail.com>
2025-03-31 00:13:05 -0700

    Snake game

--- src/SNKgame/Helpers/SNKhelpers.js
+++ src/SNKgame/Helpers/SNKhelpers.js
@@ -1,40 +1,85 @@
-export const bumpChecker = (snake) => {
+const bumped = (setSNKUser) => {
 
+    setSNKUser(prevMatrix => {
 
-    return false;
+        const newMatrix = [...prevMatrix];
+        newMatrix[0] = true;
+
+        return newMatrix;
+
+    });
 
 }
 
-export const changeSnakeDirection = (activeDirection, setSnake) => {
 
-    if (activeDirection == 0){
-    //Left 
+const bumpChecker = (activeDirection, setSNKUser, snake) => {
 
-        /*
-        setSnake(prevMatrix => {
+    const gameboardHeight = 20;
+    const gameboardWidth = 30;
 
-            const newMatrix = [...prevMatrix];
-        
-            
+    if (activeDirection === 0 && snake[0][1] === 0){
 
-            return newMatrix;
+        bumped(setSNKUser);
 
-        });
-        */
+    } else if (activeDirection === 1 && snake[0][1] === gameboardWidth-1){
 
+        bumped(setSNKUser);
 
-    } else if (activeDirection == 1){
-    //Right 
+    } else if (activeDirection === 2 && snake[0][0] === 0){
 
-       
+        bumped(setSNKUser);
 
-    } else if (activeDirection == 2){
-    //Up
+    } else if (activeDirection === 3 && snake[0][0] === gameboardHeight-1){
 
+        bumped(setSNKUser);
 
     } else {
-    //Down
 
+        if (snake.length >= 5){
+
+            const snakeHead = snake[0];
+
+            if (activeDirection === 0 && snake.some(([sx, sy]) => sx === snakeHead[0] && sy === snakeHead[1]-1)){
+        
+                bumped(setSNKUser);
+
+            } else if (activeDirection === 1 && snake.some(([sx, sy]) => sx === snakeHead[0] && sy === snakeHead[1]+1)){
+
+                bumped(setSNKUser);
+
+            } else if (activeDirection === 2 && snake.some(([sx, sy]) => sx === snakeHead[0]-1 && sy === snakeHead[1])){
+
+                bumped(setSNKUser);
+
+            } else if (activeDirection === 3 && snake.some(([sx, sy]) => sx === snakeHead[0]+1 && sy === snakeHead[1])){
+
+                bumped(setSNKUser);
+
+            }
+
+        }
+    
+    }
+
+}
+
+
+const newApple = (setAppleLocation, snake) => {
+
+    const gameboardHeight = 20;
+    const gameboardWidth = 30;
+
+    if (snake.length < (gameboardHeight * gameboardWidth)) {
+
+        let Y;
+        let X;
+    
+        do {
+            X = Math.floor(Math.random() * gameboardHeight);
+            Y = Math.floor(Math.random() * gameboardWidth);
+        } while (snake.some(([sx, sy]) => sx === X && sy === Y));
+    
+        setAppleLocation([X,Y]);
 
     }
 
@@ -42,15 +87,65 @@ export const changeSnakeDirection = (activeDirection, setSnake) => {
 
 
 
+const editSnake = (snake, setSnake, newSnakeHead, appleEaten) => {
 
+    const newMatrix = snake.map(innerArray => [...innerArray]);
 
-export const increaseSnakeLength = (snake, setSnake, appleLocation) => {
+    newMatrix[0] = newSnakeHead;
 
+    for (let i = 1; i < snake.length; i++) {
+
+        newMatrix[i] = [...snake[i - 1]];
+
+    }
+
+    if (appleEaten === true){
+
+        newMatrix.push([...snake[snake.length-1]]);
+
+    }
+
+    setSnake(prevMatrix => newMatrix);
 
 }
 
-export const newApple = (gameBoardMatrix, setAppleLocation) => {
+
+export const changeSnakeDirection = (setSNKUser, activeDirection, setSnake, snake, appleLocation, setAppleLocation) => {
+
+    bumpChecker(activeDirection, setSNKUser, snake);
+
+    let increaseSnake = false;
+
+    if (snake[0][0] === appleLocation[0] && snake[0][1] === appleLocation[1]) {
+
+        increaseSnake = true;
+        newApple(setAppleLocation, snake);
+
+    }
+
+    if (activeDirection === 0){
+    //Left 
+        
+        editSnake(snake, setSnake, [snake[0][0], snake[0][1]-1], increaseSnake);
 
 
+    } else if (activeDirection === 1){
+    //Right 
+        
+        editSnake(snake, setSnake, [snake[0][0], snake[0][1]+1], increaseSnake);
+
+
+    } else if (activeDirection === 2){
+    //Up
+
+        editSnake(snake, setSnake, [snake[0][0]-1 , snake[0][1]], increaseSnake);
+
+
+    } else if (activeDirection === 3){
+    //Down
+
+        editSnake(snake, setSnake, [snake[0][0]+1, snake[0][1]], increaseSnake);
+
+    }
 
 }

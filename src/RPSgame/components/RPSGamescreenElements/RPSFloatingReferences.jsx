5a48e66d3fba70fcb8ee44d0c2b4be66ca6f232a
Michelle Wee <michellew1822@gmail.com>
2025-04-01 13:32:09 -0700

    More styling

--- src/RPSgame/components/RPSGamescreenElements/RPSFloatingReferences.jsx
+++ src/RPSgame/components/RPSGamescreenElements/RPSFloatingReferences.jsx
@@ -5,15 +5,12 @@ import "./RPSFloatingReferences.css";
 function FloatingReferences (){
 
     const {RPSUser} = useRPSUser();
-    const level = RPSUser[0];
     const references = RPSUser[2];
-
-    
 
     return (
         <div className = "RPSFloatingReferencesContainer">
             <div className = "RPSFloatingReferencesFlag">
-                <h1 className = "RPSReferenceSign">Level {level} <span className="RPSReferenceSignGlitch">References:</span></h1>
+                <h1 className = "RPSReferenceSign">Game <span className="RPSReferenceSignGlitch">References:</span></h1>
                 <div className = "RPSReferences">
                     {references.map((item, index) => (
                             <p key={index}> &gt; {item} </p> 

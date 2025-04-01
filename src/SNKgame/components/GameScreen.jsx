1599b59e695049f03e1f1be862b08447fcf8d0c6
Michelle Wee <michellew1822@gmail.com>
2025-03-31 17:32:22 -0700

    snake!!

--- src/SNKgame/components/GameScreen.jsx
+++ src/SNKgame/components/GameScreen.jsx
@@ -1,13 +1,53 @@
+import { Link } from 'react-router-dom';
+
 import GameBoard from "./gameComponents/GameBoard.jsx";
+
+import { usePlayer } from '../../Providers/PlayerProvider.jsx';
+import { useSNKUser } from '../Providers/SNKUserProvider.jsx';
+import { useActiveGame } from '../../Providers/ActiveGameProvider.jsx';
+
+import {retrieveActiveGame} from '../../Helpers/helpers.js';
 
 function GamesScreen (){
 
+    const { ActiveGame, setActiveGame} = useActiveGame();
+    const { SNKUser, setSNKUser } = useSNKUser();
+    const { Player, setPlayer} = usePlayer();
+    
+    const reset = () => {
+    
+        setSNKUser([false, 0]);
+
+        const difference = Player[0] - 5;
+    
+        if (difference >= 0){
+    
+            setPlayer(prev => [difference, prev[0]]);
+    
+        } else {
+    
+            setPlayer(prev => [0, prev[0]]);
+    
+        }
+
+        setActiveGame(retrieveActiveGame(1))
+    
+    }
+
     return (
 
-        <div className = "screenLayout">
+        <div>
 
-            <GameBoard/>
-            
+            <Link to="/selection" className = "generalbuttonGlitch" onClick={reset}>
+                Quit Game
+            </Link>
+
+            <div className = "screenLayout">
+
+                <GameBoard/>
+                
+            </div>`
+
         </div>
 
     );
--- src/SNKgame/components/InstructionsScreen.jsx
+++ src/SNKgame/components/InstructionsScreen.jsx
@@ -1,6 +1,11 @@
 import { Link } from 'react-router-dom';
 
+import { useActiveGame } from '../../Providers/ActiveGameProvider.jsx';
+import {retrieveActiveGame} from '../../Helpers/helpers.js';
+
 function InstructionsScreen (){
+
+    const { ActiveGame, setActiveGame} = useActiveGame();
 
     return (
 
@@ -16,13 +21,21 @@ function InstructionsScreen (){
                 &gt; However, the snake cannot immediately move in the opposite direction that it is traveling in
                 at any given time and must be turned around with the necessary controls.<br/>
                 &gt; For every apple that is eaten, you will gain 2 points. <br/>
-                &gt; You will automatically give up 10 points by playing this game. <br/>
+                &gt; You will automatically give up 5 points by playing this game. <br/>
                 &gt; NOTE: Beyond this point, you will lose points if you quit in the middle of the game. <br/>
             </p>
 
-            <Link to="/SNKgame" className = "generalbuttonGlitch">
-                Go to Game
-            </Link>
+            <div className = "generalbuttonContainer">
+
+                <Link to="/selection" className = "generalbuttonGlitch" onClick={() => setActiveGame(retrieveActiveGame(1))}>
+                    Exit Game
+                </Link>
+
+                <Link to="/SNKgame" className = "generalbuttonGlitch">
+                    Go to Game
+                </Link>
+
+            </div>
             
         </div>
 

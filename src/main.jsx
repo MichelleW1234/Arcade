38e259ac84c2b47e23df0ffb1f8012b1389d1767
Michelle Wee <michellew1822@gmail.com>
2025-04-06 02:48:53 -0700

    points calculation

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 
 import { ActiveGameProvider } from './Providers/ActiveGameProvider.jsx'; 
 import { PlayerProvider } from './Providers/PlayerProvider.jsx'; 
+import { PrizeProvider } from './Providers/PrizeProvider.jsx';
 
 import { RPSUserProvider } from './RPSgame/Providers/RPSUserProvider.jsx'; 
 import {TTTUserProvider } from './TTTgame/Providers/TTTUserProvider.jsx'; 
@@ -12,10 +13,12 @@ import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <PrizeProvider>
     <PlayerProvider><ActiveGameProvider>
     <SNKUserProvider><TTTUserProvider><RPSUserProvider>
         <App />
     </RPSUserProvider></TTTUserProvider></SNKUserProvider>
     </ActiveGameProvider></PlayerProvider>
+    </PrizeProvider>
   </StrictMode>,
 )

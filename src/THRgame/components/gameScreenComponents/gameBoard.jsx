aa9667157532889c58139400c88c87406d9cdd1a
Michelle Wee <michellew1822@gmail.com>
2025-05-14 12:48:19 -0700

    laser stuff

--- src/THRgame/components/gameScreenComponents/gameBoard.jsx
+++ src/THRgame/components/gameScreenComponents/gameBoard.jsx
@@ -3,16 +3,26 @@ import { useEffect, useState } from 'react';
 
 function gameBoard({setWaveNumber, setThresholdBreached}) {
 
-    const [laserValue, setLaserValue] = useState(50);
+    const [laserValue, setLaserValue] = useState(14);
     const [laserBlasted, setLaserBlasted] = useState(-1);
 
     //15 x 27 (30x30 pieces moving around the screen)
     const gameBoardMatrix = Array.from({ length: 15 }, () => Array(27).fill(0));
-    const aliens = [[0,5], [0,9], [0,15], [0, 20], [14, 10]];
+    const alienPositions = [[0,5], [0,9], [0,15], [0, 20], [14, 10]];
 
-    /*Listener for fire button pressed */
+
+    /*Listener for lasers being shot*/
     useEffect(() => {
-        
+
+        const isInAlienColumn = alienPositions.some(
+            ([, col]) => col === laserBlasted
+        );
+                        
+        if (isInAlienColumn) {
+            console.log("There's an alien in this column!");
+            
+        }
+
         return () => {
         };
     }, []);
@@ -34,13 +44,21 @@ function gameBoard({setWaveNumber, setThresholdBreached}) {
                 {gameBoardMatrix.map((row, rowIndex) => (
                     row.map((num, colIndex) => {
 
-                        const isAlienHere = aliens.some(
+                        const isAlienHere = alienPositions.some(
                             ([alienRow, alienCol]) => alienRow === rowIndex && alienCol === colIndex
                         );
+
+                        const isLaserHere = laserBlasted === colIndex;
                         
                         return (
 
-                            isAlienHere ? (
+                            isLaserHere ? (
+
+                                <div key={rowIndex + "," + colIndex} className="THRlaserSpace">
+
+                                </div>
+
+                            ) : isAlienHere ? (
 
                                 <div key={rowIndex + "," + colIndex} className="THRalienSpace">
 
@@ -66,7 +84,7 @@ function gameBoard({setWaveNumber, setThresholdBreached}) {
             <input className = "THRSlider"
                 type="range"
                 min="0"
-                max="100"
+                max="26"
                 value={laserValue}
                 onChange={(e) => setLaserValue(e.target.value)}
             />

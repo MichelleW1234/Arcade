0e7a683fff5711581f6935c798fc3920c07be42c
Michelle Wee <michellew1822@gmail.com>
2025-05-23 15:32:20 -0700

    glitch minimization for mission 2

--- src/THRgame/components/M2components/gameScreenM2Components/gameBoardM2.jsx
+++ src/THRgame/components/M2components/gameScreenM2Components/gameBoardM2.jsx
@@ -2,19 +2,18 @@ import { useEffect, useState, useRef } from 'react';
 import AlienNormal from '../../../../Images/image 8.svg';
 import LaserBeam from '../../../../Images/image 9.svg';
 
-import {aliensIncomingM2, alienKilledM2, newWave, laserBlaster} from "../../../helpers/THRhelpers.js";
+import {aliensIncomingM2, alienKilledM2, newWave, laserBlasterM2} from "../../../helpers/THRhelpers.js";
 
 import "./gameBoardM2.css";
 
 function gameBoardM2({waveNumber, setWaveNumber, setThresholdBreached}) {
 
     const [laserValue, setLaserValue] = useState(14);
-    const [laserPositions, setLaserPositions] = useState([[14, laserValue], [13, laserValue], [12, laserValue]]);
+    const [laserPositions, setLaserPositions] = useState([[[14, laserValue], [13, laserValue], [12, laserValue], []]]);
     const [alienPositions, setAlienPositions] = useState(newWave(2));
 
     //15 x 27 (30x30 pieces moving around the screen)
     const gameBoardMatrix = Array.from({ length: 15 }, () => Array(27).fill(0));
-
 
     /*Listener for current alien positions*/
     const alienPositionsRef = useRef(alienPositions);
@@ -35,18 +34,17 @@ function gameBoardM2({waveNumber, setWaveNumber, setThresholdBreached}) {
     }, [laserValue]);
 
 
-
+    
     /*Listener for aliens being shot*/
     useEffect(() => {
 
         const interval = setInterval(() => {
-            alienKilledM2(laserPositionsRef.current, alienPositionsRef.current, setAlienPositions, setWaveNumber);
+            alienKilledM2(laserPositionsRef.current, alienPositionsRef.current, setLaserPositions, setAlienPositions, setWaveNumber);
         }, 60);
 
         return () => clearInterval(interval);
 
     }, []);
-
 
     /* Listener for wave rerendering */
     useEffect(() => {
@@ -63,7 +61,7 @@ function gameBoardM2({waveNumber, setWaveNumber, setThresholdBreached}) {
     useEffect(() => {
 
         const interval = setInterval(() => {
-            laserBlaster(laserPositionsRef.current, setLaserPositions, laserValueRef.current);
+            laserBlasterM2(laserPositionsRef.current, setLaserPositions, laserValueRef.current);
         }, 80);
 
         return () => clearInterval(interval);
@@ -88,8 +86,12 @@ function gameBoardM2({waveNumber, setWaveNumber, setThresholdBreached}) {
                             ([alienRow, alienCol, hitCount, type]) => alienRow === rowIndex && alienCol === colIndex && type === 2
                         );
 
-                        const isLaserHere = laserPositions.some(
-                            ([laserRow, laserCol]) => laserRow === rowIndex && laserCol === colIndex
+                        const isLaserHere = laserPositions.some(laser =>
+                            laser.some(element => 
+                                element.length === 2 &&   
+                                element[0] === rowIndex && 
+                                element[1] === colIndex
+                            )
                         );
 
                         return (

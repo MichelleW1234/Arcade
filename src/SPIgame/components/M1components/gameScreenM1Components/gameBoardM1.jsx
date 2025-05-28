6e3336f6c8d0afdf574d08595958710e1dac4607
Michelle Wee <michellew1822@gmail.com>
2025-05-28 03:15:01 -0700

    Changed name

---
+++ src/SPIgame/components/M1components/gameScreenM1Components/gameBoardM1.jsx
@@ -0,0 +1,139 @@
+import { useEffect, useState, useRef } from 'react';
+
+import AlienNormal from '../../../../Images/image 8.svg';
+import LaserBeam from '../../../../Images/image 9.svg';
+
+import { useSPIUser } from '../../../Providers/SPIUserProvider.jsx';
+
+import "../../gameBoard.css"
+
+import {aliensIncomingM1andM3, newWave, alienKilledM1, laserBlaster} from "../../../helpers/SPIhelpers.js";
+
+function gameBoard({waveNumber, setWaveNumber}) {
+
+    //15 x 27 (30x30 pieces moving around the screen)
+    const gameBoardMatrix = Array.from({ length: 15 }, () => Array(27).fill(0));
+
+    const {SPIUser, setSPIUser} = useSPIUser();
+
+    const [laserValue, setLaserValue] = useState(14);
+    const [laserPositions, setLaserPositions] = useState([[14, laserValue], [13, laserValue], [12, laserValue]]);
+    const [alienPositions, setAlienPositions] = useState(newWave(1));
+
+
+    /*Listener for current alien positions*/
+    const alienPositionsRef = useRef(alienPositions);
+    useEffect(() => {
+        alienPositionsRef.current = alienPositions;
+    }, [alienPositions]);
+
+    /*Listener for current laser positions*/
+    const laserPositionsRef = useRef(laserPositions);
+    useEffect(() => {
+        laserPositionsRef.current = laserPositions;
+    }, [laserPositions]);
+
+     /*Listener for current laser positions*/
+    const laserValueRef = useRef(laserValue);
+    useEffect(() => {
+        laserValueRef.current = laserValue;
+    }, [laserValue]);
+
+
+    /*Listener for aliens being shot*/
+    useEffect(() => {
+
+        const interval = setInterval(() => {
+            alienKilledM1(laserPositionsRef.current, alienPositionsRef.current, setAlienPositions, setWaveNumber);
+        }, 50);
+
+        return () => clearInterval(interval);
+
+    }, []);
+
+
+    /* Listener for wave rerendering */
+    useEffect(() => {
+
+        const interval = setInterval(() => {
+            aliensIncomingM1andM3(setAlienPositions, alienPositionsRef.current, setSPIUser);
+        }, 2000 - 200*waveNumber);
+
+        return () => clearInterval(interval);
+
+    }, [waveNumber]);
+
+     /* Listener for laser rerendering */
+    useEffect(() => {
+
+        const interval = setInterval(() => {
+            laserBlaster(laserPositionsRef.current, setLaserPositions, laserValueRef.current);
+        }, 80);
+
+        return () => clearInterval(interval);
+
+    }, []);
+
+
+    return (
+
+        <div className = "SPIgameContainer">
+
+            <div className = "SPIgameScreen"> 
+
+                {gameBoardMatrix.map((row, rowIndex) => (
+                    row.map((num, colIndex) => {
+
+                        const isAlienHere = alienPositions.some(
+                            ([alienRow, alienCol]) => alienRow === rowIndex && alienCol === colIndex
+                        );
+
+                        const isLaserHere = laserPositions.some(
+                            ([laserRow, laserCol]) => laserRow === rowIndex && laserCol === colIndex
+                        );
+
+                        return (
+
+                            isLaserHere ? (
+
+                                <div key={rowIndex + "," + colIndex} className="SPIemptySpaceM1">
+                                    <img src={LaserBeam} alt="LaserBeam" />
+                                </div>
+
+                            ) : isAlienHere ? (
+
+                                <div key={rowIndex + "," + colIndex} className="SPIemptySpaceM1">
+                                    <img src={AlienNormal} alt="Alien" />
+                                </div>
+
+
+                            ) : (
+
+                                <div key={rowIndex + "," + colIndex} className="SPIemptySpaceM1">
+                                </div>
+                    
+                            )
+                    
+                        )
+
+                    })
+                ))}
+
+                
+            </div>
+
+            <input className = "SPISlider"
+                type="range"
+                min="0"
+                max="26"
+                value={laserValue}
+                onChange={(e) => setLaserValue(Number(e.target.value))}
+            />
+
+        </div>
+
+
+    )
+
+}
+export default gameBoard
---
+++ src/SPIgame/components/missionScreen.jsx
@@ -0,0 +1,125 @@
+import { useState, useEffect} from 'react';
+import { Link } from 'react-router-dom';
+
+import { useSPIUser } from '../Providers/SPIUserProvider.jsx';
+import { useActiveGame } from '../../Providers/ActiveGameProvider.jsx';
+import { usePlayer } from '../../Providers/PlayerProvider.jsx';
+
+import "./missionScreen.css";
+
+import {playSound, retrieveActiveGame} from '../../Helpers/helpers.js';
+
+function missionScreen() {
+
+    const allMissions = [1, 2, 3, 4];
+    const missionNames = ["Surface Sweep", "Defense Protocol", "Strain X", "Operation: Kill the Queen"]
+
+    const { ActiveGame, setActiveGame} = useActiveGame();
+    const { Player, setPlayer } = usePlayer();
+    const {SPIUser, setSPIUser} = useSPIUser();
+
+    const [currGamePath, setCurrGamePath] = useState(SPIUser[1][1]);
+
+    const reset = () => {
+        
+        playSound(4);
+        setSPIUser([[],[1, "/SPIM1Instructions"], false]);
+
+        const difference = Player[0] - ActiveGame[1];
+    
+        if (difference >= 0){
+    
+            setPlayer(prev => [difference, prev[0]]);
+    
+        } else {
+    
+            setPlayer(prev => [0, prev[0]]);
+    
+        }
+
+        setActiveGame(retrieveActiveGame(1));
+        
+    }
+
+    useEffect(() => {
+        setCurrGamePath(SPIUser[1][1]); 
+    }, [SPIUser[1][1]]); 
+
+    const calculatePoints = () => {
+
+        const pointDifference = (Player[0] - ActiveGame[1]) + (SPIUser[0].length*ActiveGame[1]);
+        setPlayer(prev => [pointDifference, prev[0]]);
+
+    }
+
+    return (
+
+        <div>
+
+            <Link to="/selection" className = "generalbutton" onClick={reset}>
+                Quit Game
+            </Link>
+
+            <div className = "screenLayout">
+
+                <div className = "headerwords"> Your Mission<span className = "headerwordsGlitch">s</span>: </div>
+                <div className = "SPImissionContainer">
+                    {allMissions.map((mission, index) => (
+                        SPIUser[2] == true ? (
+
+                            <div key = {mission} className="SPImissionWindowUnavailable">
+                                
+                                <h1> X </h1>
+                            
+                            </div>
+
+                        ) : SPIUser[0].includes(mission) ? (
+                            <div key = {mission} className= "SPImissionWindowCompleted">
+                                
+                                <h1>Mission {mission} Completed</h1>
+                            
+                            </div>
+
+
+                        ) : SPIUser[1][0] == mission ? (
+
+                            <div key = {mission} className="SPImissionWindowUnlocked">
+
+                                <h1> Mission {mission}:  {missionNames[index]}</h1>
+                                <Link to= {currGamePath} className = "SPImissionStartButton">
+                                    Start
+                                </Link>
+                            
+                            </div>
+
+                        ) : (
+
+                            <div key = {mission} className="SPImissionWindowLocked">
+
+                                <h1> Locked </h1>
+                            
+                            </div>
+
+                        )
+                    ))}
+                </div>
+
+                {SPIUser[0].length == allMissions.length || SPIUser[2] == true ? (
+
+                    <Link to= "/SPIsummary" className = "generalbuttonGlitch" onClick = {() => calculatePoints()}>
+                        View Game Summary
+                    </Link>
+
+                ) : (
+
+                    null
+
+                )}
+
+            </div>
+        </div>
+
+    )
+
+}
+export default missionScreen

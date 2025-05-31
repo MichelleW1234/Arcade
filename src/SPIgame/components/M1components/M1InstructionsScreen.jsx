a4510ae7e61cb3b4819a30cb40307cbe4715c88b
Michelle Wee <michellew1822@gmail.com>
2025-05-31 13:41:42 -0700

    more editing

--- src/SPIgame/components/M1components/M1InstructionsScreen.jsx
+++ src/SPIgame/components/M1components/M1InstructionsScreen.jsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom';
 
 import alien from "../../../Images/image 8.svg";
 import laser from "../../../Images/image 9.svg";
+import laserCannon from "../../../Images/image 17.svg";
 
 import {playSound} from '../../../Helpers/helpers.js';
 
@@ -14,15 +15,15 @@ function M1InstructionsScreen() {
       <h1 className = "instructionsSign"> Station 1: Hive outskirts </h1>
 
       <p className = "largefont">   
-        &gt; Mission description: Exterminate all oncoming waves before they reach you. <br/>
-        &gt; Aliens detected:<br/>
-        &nbsp; &nbsp; &gt; <img src={alien} ></img> - soldier  <br/>
+        &gt; Mission description: Exterminate all oncoming waves before they reach you. <br/> <br/>
+        &gt; Aliens detected:<br/> <br/>
+        &nbsp; &nbsp; &gt; <img src={alien} ></img> - Soldier  <br/> <br/>
         &nbsp; &nbsp; &nbsp; &bull; Laser weakness: <img src={laser} ></img> <br/>
-        &nbsp; &nbsp; &nbsp; &bull; Special abilities: None <br/>
+        &nbsp; &nbsp; &nbsp; &bull; Special abilities: None <br/> <br/>
         &gt; Waves to eliminate: 5 <br/>
-        &gt; Wave size: 10 <br/>
-        &gt; Equipment: <br/>
-        &nbsp; &nbsp; &gt; laser cannon <br/>
+        &gt; Wave size: 10 <br/> <br/>
+        &gt; Equipment: <br/><br/>
+        &nbsp; &nbsp; &gt; <img src={laserCannon} ></img> - laser cannon <br/> <br/>
         &nbsp; &nbsp; &nbsp; &bull; ammunition: <img src={laser} ></img> <br/>
         &nbsp; &nbsp; &nbsp; &bull; To use: move slider to aim at target <br/>
         &nbsp; &nbsp; &nbsp; &bull; Unlimited ammunition<br/>
--- src/SPIgame/components/M2components/M2InstructionsScreen.jsx
+++ src/SPIgame/components/M2components/M2InstructionsScreen.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import alienArmored from "../../../Images/image 11.svg";
 import alien from "../../../Images/image 8.svg";
 import laser from "../../../Images/image 9.svg";
+import laserCannon from "../../../Images/image 17.svg";
 
 import {playSound} from '../../../Helpers/helpers.js';
 
@@ -15,19 +16,19 @@ function M2InstructionsScreen() {
       <h1 className = "instructionsSign"> Station 2: Entry </h1>
 
       <p className = "largefont">   
-        &gt; Mission description: Exterminate all oncoming waves before they reach you. <br/>
-        &gt; Aliens detected:<br/>
-        &nbsp; &nbsp; &gt; <img src={alien} ></img> - soldier  <br/>
+        &gt; Mission description: Exterminate all oncoming waves before they reach you. <br/> <br/>
+        &gt; Aliens detected:<br/> <br/>
+        &nbsp; &nbsp; &gt; <img src={alien} ></img> - Soldier  <br/> <br/>
         &nbsp; &nbsp; &nbsp; &bull; Laser weakness: <img src={laser} ></img> <br/>
-        &nbsp; &nbsp; &nbsp; &bull; Special abilities: None <br/>
-        &nbsp; &nbsp; &gt; <img src={alienArmored} ></img> - Sentinel <br/>
+        &nbsp; &nbsp; &nbsp; &bull; Special abilities: None <br/> <br/>
+        &nbsp; &nbsp; &gt; <img src={alienArmored} ></img> - Sentinel <br/> <br/>
         &nbsp; &nbsp; &nbsp; &bull; Laser weakness: RESISTANT <br/>
         &nbsp; &nbsp; &nbsp; &bull; Special abilities: Shifting positions <br/>
-        &nbsp; &nbsp; &nbsp; &bull; Other notes: Can be scared off if all other alien types are eliminated. <br/>
+        &nbsp; &nbsp; &nbsp; &bull; Other notes: Can be scared off if all other alien types are eliminated. <br/> <br/>
         &gt; Waves to eliminate: 5 <br/>
-        &gt; Wave size: 20 <br/>
-        &gt; Equipment: <br/>
-        &nbsp; &nbsp; &gt; laser cannon <br/>
+        &gt; Wave size: 20 <br/> <br/>
+        &gt; Equipment: <br/> <br/>
+        &nbsp; &nbsp; &gt;  <img src={laserCannon} ></img> - laser cannon <br/> <br/>
         &nbsp; &nbsp; &nbsp; &bull; ammunition: <img src={laser} ></img> <br/>
         &nbsp; &nbsp; &nbsp; &bull; To use: move slider to aim at target <br/>
         &nbsp; &nbsp; &nbsp; &bull; Unlimited ammunition<br/>

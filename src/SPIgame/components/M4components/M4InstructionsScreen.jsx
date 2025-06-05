6e32f2ecf9f9b26f10b6121a3598b83d987f3c79
Michelle Wee <michellew1822@gmail.com>
2025-06-05 02:41:17 -0700

    more editing

--- src/SPIgame/components/M4components/M4InstructionsScreen.jsx
+++ src/SPIgame/components/M4components/M4InstructionsScreen.jsx
@@ -17,23 +17,22 @@ function M4InstructionsScreen() {
       <h1 className = "instructionsSign"> Station 4: The Nest </h1>
 
       <p className = "largefont">   
-        &gt; Mission description: Defeat the queen <br/> <br/>
-        &gt; Aliens detected:<br/> <br/>
+        &gt; Mission Description: Defeat the queen <br/> <br/>
+        &gt; Aliens Detected:<br/> <br/>
         &nbsp; &nbsp; &gt; <img src={boss} ></img> - The Queen  <br/> <br/>
-        &nbsp; &nbsp; &nbsp; &bull; Special abilities: Shifting positions, Switching between states<br/> <br/>
-        &nbsp; &nbsp; &nbsp; &bull; Normal state: <img src={boss} ></img> - okay to shoot <br/> <br/>
-        &nbsp; &nbsp; &nbsp; &bull; Explosive state: <img src={bossDanger} ></img> - DO NOT shoot <br/> <br/>
-        &nbsp; &nbsp; &nbsp; &bull; Other notes: Do NOT let your light go out before it's dead. <br/> <br/>
+        &nbsp; &nbsp; &nbsp; &bull; Special Abilities: Shifting positions, switching between states<br/> <br/>
+        &nbsp; &nbsp; &nbsp; &bull; Normal State: <img src={boss} ></img> - Okay to shoot <br/> <br/>
+        &nbsp; &nbsp; &nbsp; &bull; Explosive State: <img src={bossDanger} ></img> - DO NOT shoot <br/> <br/>
+        &nbsp; &nbsp; &nbsp; &bull; Other Notes: Do NOT let your light go out before the Queen dies. <br/> <br/>
         &gt; Equipment: <br/> <br/>
         &nbsp; &nbsp; &gt; <img src={beamlight} ></img> - Beamlight <br/> <br/>
         &nbsp; &nbsp; &nbsp; &bull; Illuminates objects in complete darkness <br/>
-        &nbsp; &nbsp; &nbsp; &bull; Battery life: 60 seconds <br/> <br/>
-        &nbsp; &nbsp; &gt; <img src={rifle} ></img> - Laser rifle<br/> <br/>
+        &nbsp; &nbsp; &nbsp; &bull; Battery Life: 60 seconds <br/> <br/>
+        &nbsp; &nbsp; &gt; <img src={hpbar} ></img> - Health Bar <br/> <br/>
+        &nbsp; &nbsp; &nbsp; &bull; Tracks health status of enemy <br/> <br/>
+        &nbsp; &nbsp; &gt; <img src={rifle} ></img> - Laser Rifle<br/> <br/>
         &nbsp; &nbsp; &nbsp; &bull; First-person weapon designed for precision firing <br/>
-        &nbsp; &nbsp; &nbsp; &bull; To use: Aim at target and click <br/>
-        &nbsp; &nbsp; &nbsp; &bull; Unlimited ammunition <br/> <br/>
-        &nbsp; &nbsp; &gt; <img src={hpbar} ></img> - Health bar <br/> <br/>
-        &nbsp; &nbsp; &nbsp; &bull; Tracks health status of enemy <br/>
+        &nbsp; &nbsp; &nbsp; &bull; To Use: Aim at target and click <br/>
       </p>
 
       <div className = "generalbuttonContainer">

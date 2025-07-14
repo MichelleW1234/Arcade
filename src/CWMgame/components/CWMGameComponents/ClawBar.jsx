4084b5aa617424adf372eeb913f9d83dd4345908
Michelle Wee <michellew1822@gmail.com>
2025-07-14 10:53:56 -0700

    changed style

--- src/CWMgame/components/CWMGameComponents/ClawBar.jsx
+++ src/CWMgame/components/CWMGameComponents/ClawBar.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect, useRef } from "react";
 
 import "./ClawBar.css";
+import positionIndicator from "../../../Images/image 21.svg";
+
 import { playSound } from "../../../Helpers/helpers.js";
 
 function ClawBar({setButtonHit, setResult, currentPosition}) {
@@ -13,25 +15,59 @@ function ClawBar({setButtonHit, setResult, currentPosition}) {
             <div className = "CWMBar">
                 {array.map((index) => (
 
-                    currentPosition == index ? (
+                    index < 10 || index > 20 ? (
 
-                        <div key={index} className="CWMBarCurrent"></div>
+                        currentPosition == index ? (
 
-                    ) : index < 10 || index > 20 ? (
+                            <div key={index} className="CWMBarNotAccurate">
+                                <img className="CWMBarCurrent" key={index} src={positionIndicator}/>
+                            </div>
 
-                        <div key={index} className="CWMBarNotAccurate"></div>
+                        ) : (
+
+                            <div key={index} className="CWMBarNotAccurate"></div>
+
+                        )
 
                     ) : index < 13 || index > 17 ? (
 
-                        <div key={index} className="CWMBarInRange"></div>
+                        currentPosition == index ? (
+
+                            <div key={index} className="CWMBarInRange">
+                                <img className="CWMBarCurrent"  key={index} src={positionIndicator}/>
+                            </div>
+
+                        ) : (
+
+                            <div key={index} className="CWMBarInRange"></div>
+
+                        )
 
                     ) : index < 15 || index > 15 ? (
 
-                        <div key={index} className="CWMBarClose"></div>
+                        currentPosition == index ? (
+
+                            <div key={index} className="CWMBarClose">
+                                <img className="CWMBarCurrent"  key={index} src={positionIndicator}/>
+                            </div>
+
+                        ) : (
+
+                            <div key={index} className="CWMBarClose"></div>
+                        )
 
                     ) : (
 
-                        <div key={index} className="CWMBarVeryClose"></div>
+                        currentPosition == index ? (
+
+                             <div key={index} className="CWMBarVeryClose">
+                                <img className="CWMBarCurrent"  key={index} src={positionIndicator}/>
+                            </div>
+
+                        ) : (
+
+                            <div key={index} className="CWMBarVeryClose"></div>
+                        )
 
                     )
                     

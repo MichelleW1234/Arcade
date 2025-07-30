cdbb40b9db18f766f517dd1f086340f5fec9b4dd
Michelle Wee <michellew1822@gmail.com>
2025-07-30 14:49:04 -0700

    finally fixed zoom??

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
-import { app, BrowserWindow, screen} from 'electron';
+import { app, BrowserWindow} from 'electron';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
 // Required to construct __dirname in ES module style
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
-// Define zoom globally
-let adjustedZoom = 0.7;
 
 function createWindow () {
   const win = new BrowserWindow({
@@ -27,25 +24,7 @@ function createWindow () {
     win.loadFile(path.join(__dirname, 'dist', 'index.html'));
   }
 
-
-  /*Accounting for consistency in zoom across different machines: */
-  const scaleFactor = screen.getPrimaryDisplay().scaleFactor;     // Get screen scale (DPI factor)
-  const baseZoom = 0.7;    // Define a base zoom
-  adjustedZoom = baseZoom / scaleFactor;    // Adjust for screen DPI
-
-
-  win.webContents.on('did-finish-load', () => {
-    win.webContents.setZoomFactor(adjustedZoom);
-  });
-
 }
-
-app.on('browser-window-focus', () => {
-  const win = BrowserWindow.getFocusedWindow();
-  if (win) {
-    win.webContents.setZoomFactor(adjustedZoom);
-  }
-});
 
 app.whenReady().then(createWindow);
 

e53c4898f3c73f5953a40238d4c78853fafb8cee
Michelle Wee <michellew1822@gmail.com>
2025-09-03 20:05:37 -0700

    css

--- src/App.jsx
+++ src/App.jsx
@@ -79,85 +79,85 @@ function App() {
   const { Termination, setTermination } = useTermination();
 
   return (
-    <div className = "innerboarder">
-    <HashRouter>
-      <ScrollToTop />
-      <Routes>
-        <Route index element={Termination[0] ? <ArcadeStartscreen /> : <Continuationscreen />}/>
-        
-        <Route path="/arcadeStart" element={<ArcadeStartscreen />}/>
-        <Route path="/continuation" element={<Continuationscreen />}/>
-        <Route path="/rulesAndPoints" element={<RulesandPointsscreen />}/>
-        <Route path="/prizeRoom" element={<PrizeRoomscreen />}/>
-        <Route path="/selection" element={<GameSelectionscreen />}/>
+    <div>
+      <HashRouter>
+        <ScrollToTop />
+        <Routes>
+          <Route index element={Termination[0] ? <ArcadeStartscreen /> : <Continuationscreen />}/>
+          
+          <Route path="/arcadeStart" element={<ArcadeStartscreen />}/>
+          <Route path="/continuation" element={<Continuationscreen />}/>
+          <Route path="/rulesAndPoints" element={<RulesandPointsscreen />}/>
+          <Route path="/prizeRoom" element={<PrizeRoomscreen />}/>
+          <Route path="/selection" element={<GameSelectionscreen />}/>
 
-        <Route path="/CWMstart" element={<CWMStartscreen />}/>
-        <Route path="/CWMinstructions" element={<CWMInstructionsscreen />}/>
-        <Route path="/CWMselection" element={<CWMMachineSelectionscreen />}/>
-        <Route path="/CWMcatinstructions" element={<CWMCatInstructionsscreen />}/>
-        <Route path="/CWMcatgame" element={<CWMCatGamescreen />}/>
-        <Route path="/CWMcatsummary" element={<CWMCatSummaryscreen />}/>
-        <Route path="/CWMsportsinstructions" element={<CWMSportsInstructionsscreen />}/>
-        <Route path="/CWMsportsgame" element={<CWMSportsGamescreen />}/>
-        <Route path="/CWMsportssummary" element={<CWMSportsSummaryscreen />}/>
-        <Route path="/CWMspaceinstructions" element={<CWMSpaceInstructionsscreen />}/>
-        <Route path="/CWMspacegame" element={<CWMSpaceGamescreen />}/>
-        <Route path="/CWMspacesummary" element={<CWMSpaceSummaryscreen />}/>
-        
-        <Route path="/RPSstart" element={<RPSStartscreen />}/>
-        <Route path="/RPSinstructions" element={<RPSInstructionsscreen />}/>
-        <Route path="/RPSlevels" element={<RPSLevelSelectionscreen />}/>
-        <Route path="/RPSgame" element={<RPSGamescreen />}/>
-        <Route path="/RPSsummary" element={<RPSSummaryscreen />}/>
+          <Route path="/CWMstart" element={<CWMStartscreen />}/>
+          <Route path="/CWMinstructions" element={<CWMInstructionsscreen />}/>
+          <Route path="/CWMselection" element={<CWMMachineSelectionscreen />}/>
+          <Route path="/CWMcatinstructions" element={<CWMCatInstructionsscreen />}/>
+          <Route path="/CWMcatgame" element={<CWMCatGamescreen />}/>
+          <Route path="/CWMcatsummary" element={<CWMCatSummaryscreen />}/>
+          <Route path="/CWMsportsinstructions" element={<CWMSportsInstructionsscreen />}/>
+          <Route path="/CWMsportsgame" element={<CWMSportsGamescreen />}/>
+          <Route path="/CWMsportssummary" element={<CWMSportsSummaryscreen />}/>
+          <Route path="/CWMspaceinstructions" element={<CWMSpaceInstructionsscreen />}/>
+          <Route path="/CWMspacegame" element={<CWMSpaceGamescreen />}/>
+          <Route path="/CWMspacesummary" element={<CWMSpaceSummaryscreen />}/>
+          
+          <Route path="/RPSstart" element={<RPSStartscreen />}/>
+          <Route path="/RPSinstructions" element={<RPSInstructionsscreen />}/>
+          <Route path="/RPSlevels" element={<RPSLevelSelectionscreen />}/>
+          <Route path="/RPSgame" element={<RPSGamescreen />}/>
+          <Route path="/RPSsummary" element={<RPSSummaryscreen />}/>
 
-        <Route path="/TTTstart" element={<TTTStartscreen  />}/>
-        <Route path="/TTTinstructions" element={<TTTInstructionsscreen />}/>
-        <Route path="/TTTcoinflip" element={<TTTTurnDecidingscreen />}/>
-        <Route path="/TTTgame" element={<TTTGamescreen />}/>
-        <Route path="/TTTsummary" element={<TTTSummaryscreen />}/>
+          <Route path="/TTTstart" element={<TTTStartscreen  />}/>
+          <Route path="/TTTinstructions" element={<TTTInstructionsscreen />}/>
+          <Route path="/TTTcoinflip" element={<TTTTurnDecidingscreen />}/>
+          <Route path="/TTTgame" element={<TTTGamescreen />}/>
+          <Route path="/TTTsummary" element={<TTTSummaryscreen />}/>
 
-        <Route path="/SNKstart" element={<SNKStartscreen />}/>
-        <Route path="/SNKinstructions" element={<SNKInstructionsscreen />}/>
-        <Route path="/SNKgame" element={<SNKGamescreen />}/>
-        <Route path="/SNKsummary" element={<SNKSummaryscreen />}/>
+          <Route path="/SNKstart" element={<SNKStartscreen />}/>
+          <Route path="/SNKinstructions" element={<SNKInstructionsscreen />}/>
+          <Route path="/SNKgame" element={<SNKGamescreen />}/>
+          <Route path="/SNKsummary" element={<SNKSummaryscreen />}/>
 
-        <Route path="/SPIstart" element={<SPIStartscreen />}/>
-        <Route path="/SPIinstructions" element={<SPIInstructionsscreen />}/>
-        <Route path="/SPImission" element={<SPIMissionscreen />}/>
-        <Route path="/SPIM1instructions" element={<SPIM1Instructionsscreen />}/>
-        <Route path="/SPIM2instructions" element={<SPIM2Instructionsscreen />}/>
-        <Route path="/SPIM3instructions" element={<SPIM3Instructionsscreen />}/>
-        <Route path="/SPIM4instructions" element={<SPIM4Instructionsscreen />}/>
-        <Route path="/SPIM1game" element={<SPIM1Gamescreen />}/>
-        <Route path="/SPIM2game" element={<SPIM2Gamescreen />}/>
-        <Route path="/SPIM3game" element={<SPIM3Gamescreen />}/>
-        <Route path="/SPIM4game" element={<SPIM4Gamescreen />}/>
-        <Route path="/SPIsummary" element={<SPISummaryscreen />}/>
+          <Route path="/SPIstart" element={<SPIStartscreen />}/>
+          <Route path="/SPIinstructions" element={<SPIInstructionsscreen />}/>
+          <Route path="/SPImission" element={<SPIMissionscreen />}/>
+          <Route path="/SPIM1instructions" element={<SPIM1Instructionsscreen />}/>
+          <Route path="/SPIM2instructions" element={<SPIM2Instructionsscreen />}/>
+          <Route path="/SPIM3instructions" element={<SPIM3Instructionsscreen />}/>
+          <Route path="/SPIM4instructions" element={<SPIM4Instructionsscreen />}/>
+          <Route path="/SPIM1game" element={<SPIM1Gamescreen />}/>
+          <Route path="/SPIM2game" element={<SPIM2Gamescreen />}/>
+          <Route path="/SPIM3game" element={<SPIM3Gamescreen />}/>
+          <Route path="/SPIM4game" element={<SPIM4Gamescreen />}/>
+          <Route path="/SPIsummary" element={<SPISummaryscreen />}/>
 
-        <Route path="/ORBstart" element={<ORBStartscreen />}/>
-        <Route path="/ORBinstructions" element={<ORBInstructionsscreen />}/>
-        <Route path="/ORBgame" element={<ORBGamescreen />}/>
-        <Route path="/ORBsummary" element={<ORBSummaryscreen />}/>
+          <Route path="/ORBstart" element={<ORBStartscreen />}/>
+          <Route path="/ORBinstructions" element={<ORBInstructionsscreen />}/>
+          <Route path="/ORBgame" element={<ORBGamescreen />}/>
+          <Route path="/ORBsummary" element={<ORBSummaryscreen />}/>
 
-        <Route path="/CBLstart" element={<CBLStartscreen />}/>
-        <Route path="/CBLinstructions" element={<CBLInstructionsscreen />}/>
-        <Route path="/CBLgame" element={<CBLGamescreen />}/>
-        <Route path="/CBLsummary" element={<CBLSummaryscreen />}/>
+          <Route path="/CBLstart" element={<CBLStartscreen />}/>
+          <Route path="/CBLinstructions" element={<CBLInstructionsscreen />}/>
+          <Route path="/CBLgame" element={<CBLGamescreen />}/>
+          <Route path="/CBLsummary" element={<CBLSummaryscreen />}/>
 
-        <Route path="/BFRstart" element={<BFRStartscreen />}/>
-        <Route path="/BFRinstructions" element={<BFRInstructionsscreen />}/>
-        <Route path="/BFRgame" element={<BFRGamescreen />}/>
-        <Route path="/BFRsummary" element={<BFRSummaryscreen />}/>
+          <Route path="/BFRstart" element={<BFRStartscreen />}/>
+          <Route path="/BFRinstructions" element={<BFRInstructionsscreen />}/>
+          <Route path="/BFRgame" element={<BFRGamescreen />}/>
+          <Route path="/BFRsummary" element={<BFRSummaryscreen />}/>
 
-        <Route path="/SMZstart" element={<SMZStartscreen />}/>
-        <Route path="/SMZinstructions" element={<SMZInstructionsscreen />}/>
-        <Route path="/SMZgame" element={<SMZGamescreen />}/>
-        <Route path="/SMZsummary" element={<SMZSummaryscreen />}/>
-        
-        {/*Page doesn't exist error*/}
-        <Route path="*" element={<NoPage />} />
-      </Routes>
-    </HashRouter>
+          <Route path="/SMZstart" element={<SMZStartscreen />}/>
+          <Route path="/SMZinstructions" element={<SMZInstructionsscreen />}/>
+          <Route path="/SMZgame" element={<SMZGamescreen />}/>
+          <Route path="/SMZsummary" element={<SMZSummaryscreen />}/>
+          
+          {/*Page doesn't exist error*/}
+          <Route path="*" element={<NoPage />} />
+        </Routes>
+      </HashRouter>
     </div>
   )
 }
